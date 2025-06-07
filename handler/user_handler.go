e18b87e09d1ceb430f591819c383a0651e7b53b5
elkeamanan <salmanmarsha9@gmail.com>
2025-06-07 19:13:02 +0700

    refactor: unified error http response

--- handler/user_handler.go
+++ handler/user_handler.go
@@ -4,6 +4,7 @@ import (
 	"context"
 	userDomain "elkeamanan/shortina/internal/user/domain"
 	"encoding/json"
+	"fmt"
 	"net/http"
 
 	"github.com/google/uuid"
@@ -16,13 +17,21 @@ func (s *Server) handlerRegisterUser(w http.ResponseWriter, r *http.Request) {
 	req := &userDomain.RegisterUserRequest{}
 	err := json.NewDecoder(r.Body).Decode(&req)
 	if err != nil {
-		http.Error(w, err.Error(), http.StatusBadRequest)
+		w.WriteHeader(http.StatusBadRequest)
+		json.NewEncoder(w).Encode(ErrorResponse{
+			ErrorCode: 400,
+			Message:   fmt.Sprintf("failed to decode request with err: %s", err.Error()),
+		})
 		return
 	}
 
 	err = s.userService.RegisterUser(ctx, req)
 	if err != nil {
-		http.Error(w, err.Error(), http.StatusInternalServerError)
+		w.WriteHeader(http.StatusInternalServerError)
+		json.NewEncoder(w).Encode(ErrorResponse{
+			ErrorCode: 500,
+			Message:   fmt.Sprintf("failed to register user with err: %s", err.Error()),
+		})
 		return
 	}
 
@@ -35,20 +44,32 @@ func (s *Server) handlerLoginUser(w http.ResponseWriter, r *http.Request) {
 	req := &userDomain.LoginUserRequest{}
 	err := json.NewDecoder(r.Body).Decode(&req)
 	if err != nil {
-		http.Error(w, err.Error(), http.StatusBadRequest)
+		w.WriteHeader(http.StatusBadRequest)
+		json.NewEncoder(w).Encode(ErrorResponse{
+			ErrorCode: 400,
+			Message:   fmt.Sprintf("failed to decode request with err: %s", err.Error()),
+		})
 		return
 	}
 
 	tokenPair, err := s.userService.LoginUser(ctx, req)
 	if err != nil {
-		http.Error(w, err.Error(), http.StatusInternalServerError)
+		w.WriteHeader(http.StatusInternalServerError)
+		json.NewEncoder(w).Encode(ErrorResponse{
+			ErrorCode: 500,
+			Message:   fmt.Sprintf("failed to login user with err: %s", err.Error()),
+		})
 		return
 	}
 
 	w.Header().Set("Content-Type", "application/json")
 	jsonData, err := json.Marshal(tokenPair)
 	if err != nil {
-		http.Error(w, err.Error(), http.StatusInternalServerError)
+		w.WriteHeader(http.StatusInternalServerError)
+		json.NewEncoder(w).Encode(ErrorResponse{
+			ErrorCode: 500,
+			Message:   fmt.Sprintf("failed to marshal response with err: %s", err.Error()),
+		})
 		return
 	}
 
@@ -64,20 +85,32 @@ func (s *Server) handlerUpdateUser(w http.ResponseWriter, r *http.Request) {
 	id := vars["id"]
 
 	if id != actorUserId {
-		http.Error(w, "cannot update user information of another user", http.StatusUnauthorized)
+		w.WriteHeader(http.StatusForbidden)
+		json.NewEncoder(w).Encode(ErrorResponse{
+			ErrorCode: 403,
+			Message:   "cannot update user information of another user",
+		})
 		return
 	}
 
 	req := &userDomain.UpdateUserRequest{}
 	err := json.NewDecoder(r.Body).Decode(&req)
 	if err != nil {
-		http.Error(w, err.Error(), http.StatusBadRequest)
+		w.WriteHeader(http.StatusBadRequest)
+		json.NewEncoder(w).Encode(ErrorResponse{
+			ErrorCode: 400,
+			Message:   fmt.Sprintf("failed to decode request with err: %s", err.Error()),
+		})
 		return
 	}
 
 	err = s.userService.UpdateUser(ctx, uuid.MustParse(id), req)
 	if err != nil {
-		http.Error(w, err.Error(), http.StatusInternalServerError)
+		w.WriteHeader(http.StatusInternalServerError)
+		json.NewEncoder(w).Encode(ErrorResponse{
+			ErrorCode: 500,
+			Message:   fmt.Sprintf("failed to update user with err: %s", err.Error()),
+		})
 		return
 	}
 
@@ -89,20 +122,32 @@ func (s *Server) handlerRefreshToken(w http.ResponseWriter, r *http.Request) {
 	ctx := context.Background()
 	refreshToken := r.URL.Query().Get("refresh_token")
 	if refreshToken == "" {
-		http.Error(w, "refresh token is required", http.StatusBadRequest)
+		w.WriteHeader(http.StatusBadRequest)
+		json.NewEncoder(w).Encode(ErrorResponse{
+			ErrorCode: 400,
+			Message:   "refresh token is required",
+		})
 		return
 	}
 
 	tokenPair, err := s.userService.RefreshUserToken(ctx, refreshToken)
 	if err != nil {
-		http.Error(w, err.Error(), http.StatusInternalServerError)
+		w.WriteHeader(http.StatusInternalServerError)
+		json.NewEncoder(w).Encode(ErrorResponse{
+			ErrorCode: 500,
+			Message:   fmt.Sprintf("failed to refresh user token with err: %s", err.Error()),
+		})
 		return
 	}
 
 	w.Header().Set("Content-Type", "application/json")
 	jsonData, err := json.Marshal(tokenPair)
 	if err != nil {
-		http.Error(w, err.Error(), http.StatusInternalServerError)
+		w.WriteHeader(http.StatusInternalServerError)
+		json.NewEncoder(w).Encode(ErrorResponse{
+			ErrorCode: 500,
+			Message:   fmt.Sprintf("failed to marshal response with err: %s", err.Error()),
+		})
 		return
 	}
 
